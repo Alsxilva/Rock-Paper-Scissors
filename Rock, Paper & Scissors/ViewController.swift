91177cef1ebf2f0df65947d248997c469b2f3aee
Alejandro Silva <alsxilva@gmail.com>
2023-10-14 13:20:10 -0600

    new App

--- Rock, Paper & Scissors/ViewController.swift
+++ Rock, Paper & Scissors/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func playersNameField(_ sender: UITextField) {
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
